31a10ac5662ac21a7080a16c6b6040870b634c3a
Joe S. Boyle <joe.boyle@blackcowtech.uk>
2022-03-25 17:13:38 +0000

    add largest prime factor solution

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 ﻿using static QuestionOneSolutions;
 using static QuestionTwoSolutions;
+using static QuestionThreeSolutions;
+
 
 
 class Solutions {
     static void Main (string[] args) {
         // Question 1 solutions
-        Console.WriteLine(Q1SolutionOne().ToString());
-        Console.WriteLine(Q1SolutionTwo().ToString());
-        Console.WriteLine(Q1SolutionThree().ToString());
+        Console.WriteLine(Q1SolutionOne());
+        Console.WriteLine(Q1SolutionTwo());
+        Console.WriteLine(Q1SolutionThree());
 
         // Question 2 solutions
         int n = 4000000;
-        Console.WriteLine(SumEvenFibonnaciNumbers1(n).ToString());
-        Console.WriteLine(SumEvenFibonnaciNumbers2(n).ToString());
-        Console.WriteLine(SumEvenFibonnaciNumbers3(n).ToString());
+        Console.WriteLine(SumEvenFibonnaciNumbers1(n));
+        Console.WriteLine(SumEvenFibonnaciNumbers2(n));
+        Console.WriteLine(SumEvenFibonnaciNumbers3(n));
+
+        // Question 3 Solutions
+        long m = 600851475143;
+        Console.WriteLine(LargestPrimeFactor(m));
+
+
     }
 }
---
+++ QuestionThree.cs
@@ -0,0 +1,74 @@
+using System;
+public static class QuestionThreeSolutions
+{
+    /*
+    Compute the largest prime factor of n
+
+    Memory: O(1) - only the current prime is stored
+    Warning: not sure this runtime is correct
+    Runtime: O(n^(5/2)) - repeat the calculation of the next prime, O(n^(3/2)), `n` times
+    */
+    public static long LargestPrimeFactor(long n)
+    {
+        long prime = nextPrime(1);
+        long limit = (long)Math.Sqrt(n);
+        while (true)
+        {
+            if (n % prime == 0) // prime is a factor of n
+            {
+                long otherFactor = n / prime;
+                if (isPrime(otherFactor))
+                {
+                    return otherFactor;
+                }
+                else
+                {
+                    return LargestPrimeFactor(otherFactor);
+                }
+            }
+            if (prime > limit)
+            {
+                // `n` must be prime and it's own largest prime factor
+                // (all positive integers greater than 1 have a prime factor.)
+                return n;
+            }
+            prime = nextPrime(prime);
+        }
+    }
+
+    /*
+    Find the next prime number following `n`
+
+    Memory: O(1) - only the candidate number is stored
+    Warning: not sure this runtime is correct
+    Runtime: O(√n * ln(n)) - check each number (O(√n)) and each check takes up to O(√n)
+    */
+    private static long nextPrime(long n)
+    {
+        long candidate = n + 1;
+
+        while (!isPrime(candidate))
+        {
+            candidate += 1;
+        }
+        return candidate;
+    }
+
+    /*
+    Return whether or not the number is a prime
+    
+    Memory: O(1) - only the candidate number is stored
+    Runtime: O(√n) - worst case loops from 2 to √n
+    */
+    private static bool isPrime(long candidate)
+    {
+        for (long i = 2; i < Math.Sqrt(candidate); ++i)
+        {
+            if (candidate % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
